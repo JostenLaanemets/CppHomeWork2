93a6c3914cf59b4599143dae88274b0786fbd9b8
Roman Gorislavski <16970738+ParadineG@users.noreply.github.com>
2023-10-02 17:23:03 +0300

    Update

--- Assignment1/assignment1.cpp
+++ Assignment1/assignment1.cpp
@@ -12,28 +12,28 @@ bool RG::Homework2::assignment1()
 
     int i{ 5 };
     // Loo muutuja ilr1 mis on int lreference tüüpi ja i väärtusega
-    int& ilr1{ i };
+
     // Loo muutuja irr mis on int rreference tüüpi ja väärtusega 5
-    int&& irr{ 2 };
+
     // Loo muutuja ilr2 mis on int lreference tüüpi ja väärtusega i
-    int& ilr2{ irr };
+
     // Loo muutuja ip mis on int pointer tüüpi ja väärtusega i
-    int* ip{ &i };
+
     // Loo muutuja ipp mis on int pointer pointer tüüpi ja väärtusega ip
-    int** ipp{ &ip };
+
     // Loo muutuja i2 mis on int tüüpi ja väärtusega millele ip viitab 
-    int i2{ *ip };
+
     // Loo muutuja up mis on ainulaadne smartpointer tüüpi ja väärtusega 4
-    std::unique_ptr up{ std::make_unique<int>(4) };
+
     // Loo muutuja sp mis on jagatud smartpointer tüüpi ja väärtusega 3
-    std::shared_ptr sp{ std::make_shared<int>(3) };
+
     // Loo muutuja wp mis on nõrk smartpointer tüüpi ja seosta sp-ga
-    std::weak_ptr wp{ sp };
+
     // Loo muutuja up mis on raw pointer tüüpi, väärtusega 6, 
     // siis kustuta see ning seejärel määra nullptr.
-    int* rp{ new int{6} };
-    delete rp;
-    rp = nullptr;
+
+
+
 
     // Kontroll, et muutujad on õiget tüüpi ja väärtusega
     return (typeid(ilr1) == typeid(int&))
